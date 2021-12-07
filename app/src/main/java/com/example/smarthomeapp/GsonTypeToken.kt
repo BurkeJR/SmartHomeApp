b5670de388d65f2d1b3499433ed081a7a360bdd3
Luke Greenway <greenwaylj18@gcc.edu>
2021-12-07 15:59:49 -0500

    Media will get info from smartHome

---
+++ app/src/main/java/com/example/smarthomeapp/GsonTypeToken.kt
@@ -0,0 +1,7 @@
+package com.example.smarthomeapp
+
+
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+inline fun <reified T> Gson.fromJson(json: String) = fromJson<T>(json, object: TypeToken<T>() {}.type)
