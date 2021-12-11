5eda1058a9da323f30bc3b2c08f3981348638974
Luke Greenway <greenwaylj18@gcc.edu>
2021-12-11 13:02:51 -0500

    You will need to update your own strings.xl. PLEASE DO NOT PUSH YOUR OWN STRINGS.XML

--- app/src/main/java/com/example/smarthomeapp/MediaDetailsFragment.kt
+++ app/src/main/java/com/example/smarthomeapp/MediaDetailsFragment.kt
@@ -9,11 +9,15 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
 import com.android.volley.Request
 import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.smarthomeapp.databinding.FragmentMediaDetailsBinding
 import com.example.smarthomeapp.databinding.FragmentMediaListBinding
 import com.google.gson.Gson
+import org.json.JSONException
+import org.json.JSONObject
 
 
 class MediaDetailsFragment : Fragment() {
@@ -70,37 +74,10 @@ class MediaDetailsFragment : Fragment() {
         return binding.root
     }
 
-    fun playMusic(){
-
-        requestQueue = Volley.newRequestQueue(this.context)
-
-        val ipAddress = getString(R.string.myIPAddress)
-        val url = "http://$ipAddress/media-players"
+    fun playMusic() {
 
 
 
-
-        val stringRequest = StringRequest(
-            Request.Method.POST,
-            url,
-            {
-                val gson = Gson()
-
-                val mediaList = gson.fromJson<ArrayResult<mediaPlayer>>(it).result
-
-                mediaList[id].isPlaying = true
-                mediaList[id].nowPlayingSongId = (songID + 1) % 5
-                gson.toJson(mediaList)
-
-            },
-            {
-                // Handle error
-                Log.e("VOLLEY", "Failed to load song list $it")
-            }
-        )
-        stringRequest.tag = this
-
-        requestQueue.add(stringRequest)
 
     }
 
---
+++ app/src/main/java/com/example/smarthomeapp/StringRequestWithBody.kt
@@ -0,0 +1,16 @@
+package com.example.learnsmarthome
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import com.google.gson.Gson
+class StringRequestWithBody(url: String, body: Any, onSuccess: (result: String) -> Unit, onError: (error: VolleyError) -> Unit)
+    : StringRequest(Method.POST, url, onSuccess, onError)
+{
+    private val stringBody: String
+    init {
+        val gson = Gson()
+        stringBody = gson.toJson(body)
+    }
+    override fun getBody(): ByteArray {
+        return stringBody.toByteArray()
+    }
+}
